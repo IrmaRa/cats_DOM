c489b01841b844570acd1d723dc1768fcf990159
Irma Rasikeviciute <rasikeviciute@yahoo.com>
2017-06-19 21:00:01 +0100

    Remove the cat data in the HTML and add via js

--- public/app.js
+++ public/app.js
@@ -42,7 +42,10 @@ var addCat = function(name, food, imgUrl) {
 }
 
 var app = function() {
-  addCat("Name: Lola", "Favourite food: Salmon", "http://londonbeep.com/wp-content/uploads/2015/04/lazy-cat-tries-to-help-9.jpg")
+  addCat("Name: Lola", "Favourite food: Salmon", "http://londonbeep.com/wp-content/uploads/2015/04/lazy-cat-tries-to-help-9.jpg");
+  addCat("Name: Boba", "Favourite food: Sock fluff", "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg");
+  addCat("Name: Barnaby", "Favourite food: Tuna", "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg");
+  addCat("Name: Max", "Favourite food: Whiskas Temptations", "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg");
 }
 
 // var app = function() {
@@ -74,14 +77,5 @@ var app = function() {
 window.onload = app;
 
 
-
-
-
-
-
-
-
-
-// 2. Refactor the code to make it easy to add a new cat, just like we did with the quotes.
 // 3. EXTENSION: Remove the cat data in the HTML and add them via your javascript. If you want to be fancy, put the cat data into an array.
 // 4. EXTENSION: Style the site!
