28fb1c6c0030f643d1894f7e71c85c7517509e57
Irma Rasikeviciute <rasikeviciute@yahoo.com>
2017-06-19 20:38:29 +0100

    Refactor the code to make it easy to add a new cat

--- public/app.js
+++ public/app.js
@@ -1,33 +1,74 @@
-// var app = function() {
-//   addCat("Name: Lola", "Favourite food: Salmon", "")
-// }
-
-
-
-var app = function() {
-
+createCatUl = function() {
   var catUl = document.createElement('ul');
   catUl.classList.add('cat');
+  return catUl;
+}
 
+createName = function(name) {
   var nameLi = document.createElement('li');
-  nameLi.innerText = "Name: Boba";
+  nameLi.innerText = name;
+  return nameLi;
+}
 
-  catUl.appendChild(nameLi);
-
+createFood = function(food) {
   var foodLi = document.createElement('li');
-  foodLi.innerText = "Favourite food: Sock fluff";
+  foodLi.innerText = food;
+  return foodLi;
+}
 
-  catUl.appendChild(foodLi);
+createImg = function(imgUrl) {
+  var img = document.createElement('li');
+  img.innerHTML = "<img width='500' src=" + imgUrl + ">";
+  return img;
+}
 
-  var img = document.createElement('image');
-  img.innerHTML = "<img width='500' src='http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg'>";
-
+appendElements = function(catList, catUl, name, food, img) {
+  catUl.appendChild(name);
+  catUl.appendChild(food);
   catUl.appendChild(img);
-
-  var catList = document.querySelector('#cats');
-
   catList.appendChild(catUl);
 }
+
+
+var addCat = function(name, food, imgUrl) {
+  var catList = document.querySelector('#cats')
+  var catUl = createCatUl();
+  var name = createName(name);
+  var food = createFood(food);
+  var img = createImg(imgUrl);
+
+  
+  appendElements(catList, catUl, name, food, img);
+}
+
+var app = function() {
+  addCat("Name: Lola", "Favourite food: Salmon", "http://londonbeep.com/wp-content/uploads/2015/04/lazy-cat-tries-to-help-9.jpg")
+}
+
+// var app = function() {
+
+//   var catUl = document.createElement('ul');
+//   catUl.classList.add('cat');
+
+  // var nameLi = document.createElement('li');
+  // nameLi.innerText = "Name: Boba";
+
+//   catUl.appendChild(nameLi);
+
+//   var foodLi = document.createElement('li');
+//   foodLi.innerText = "Favourite food: Sock fluff";
+
+//   catUl.appendChild(foodLi);
+
+//   var img = document.createElement('li');
+//   img.innerHTML = "<img width='500' src='http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg'>";
+
+//   catUl.appendChild(img);
+
+//   var catList = document.querySelector('#cats');
+
+//   catList.appendChild(catUl);
+// }
 
 
 window.onload = app;
@@ -41,9 +82,6 @@ window.onload = app;
 
 
 
-
-
-// 1. Add JS code to allow a new cat to be added programmatically. Do it rough and ready at first!
 // 2. Refactor the code to make it easy to add a new cat, just like we did with the quotes.
 // 3. EXTENSION: Remove the cat data in the HTML and add them via your javascript. If you want to be fancy, put the cat data into an array.
 // 4. EXTENSION: Style the site!
